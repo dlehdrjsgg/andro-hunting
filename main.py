77114a26d67b826821a95f1499cf55106e66959d
h0pler <h0pler@proton.me>
2024-07-10 09:46:43 +0900

    feat: implementation

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 # main.py
+from modules.run import multiRun
+from modules.file_handler.text import readText
+
+
+def main():
+    al = readText("applist.txt")
+    # al = ["com.glyde.app.android"]
+    multiRun(al)
+
+
+if __name__ == "__main__":
+    # if len(sys.argv) != 2:
+    #     print("Usage: python main.py <package_name>")
+    #     sys.exit(1)
+
+    # package_name = sys.argv[1]
+    main()
